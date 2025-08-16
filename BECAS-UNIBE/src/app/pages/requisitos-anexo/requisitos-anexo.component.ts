3ba45dfacfaf7b326aee7ef74693c7df8fb366e1
grmvera <grm.vera@yavirac.edu.ec>
2025-08-16 17:52:44 -0500

    creacion y enrutamiento de nuevo componente para requisitos del componente

---
+++ BECAS-UNIBE/src/app/pages/requisitos-anexo/requisitos-anexo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-requisitos-anexo',
+  imports: [],
+  templateUrl: './requisitos-anexo.component.html',
+  styleUrl: './requisitos-anexo.component.css'
+})
+export class RequisitosAnexoComponent {
+
+}
