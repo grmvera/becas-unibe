81a11477ca08d05916feaaa6f772e115d2459ea7
grmvera <grm.vera@yavirac.edu.ec>
2025-08-24 21:50:43 -0500

    creacion de modal para asiganr porcentaje de beca a carrerra

--- BECAS-UNIBE/src/app/pages/porcentaje-carrera/porcentaje-carrera.component.ts
+++ BECAS-UNIBE/src/app/pages/porcentaje-carrera/porcentaje-carrera.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { CrearCarreraDialogComponent, CrearCarreraResultado } from './crear-carrera-dialog.component';
+import { AsignarPorcentajeDialogComponent, AsignarPorcentajeResultado } from './asignar-porcentaje-dialog.component';
 
 @Component({
   selector: 'app-porcentaje-carrera',
@@ -8,8 +9,7 @@ import { CrearCarreraDialogComponent, CrearCarreraResultado } from './crear-carr
   styleUrls: ['./porcentaje-carrera.component.css']
 })
 export class PorcentajeCarreraComponent {
-
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog) {}
 
   abrirCrearCarrera() {
     const ref = this.dialog.open(CrearCarreraDialogComponent, {
@@ -17,12 +17,23 @@ export class PorcentajeCarreraComponent {
       maxWidth: '100%',
       disableClose: true,
       autoFocus: false,
-
     });
-
     ref.afterClosed().subscribe((resultado?: CrearCarreraResultado) => {
       if (!resultado) return;
       console.log('Nueva carrera creada:', resultado);
+    });
+  }
+
+  abrirAsignarPorcentaje() {
+    const ref = this.dialog.open(AsignarPorcentajeDialogComponent, {
+      width: '520px',
+      maxWidth: '96vw',
+      disableClose: true,
+      autoFocus: false,
+    });
+    ref.afterClosed().subscribe((resultado?: AsignarPorcentajeResultado) => {
+      if (!resultado) return;
+      console.log('Porcentaje asignado:', resultado);
     });
   }
 }
